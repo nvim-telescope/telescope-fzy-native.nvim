3cad1a0856920c8964727774e385a64426b4e93e
TJ DeVries <devries.timothyj@gmail.com>
2020-11-23 09:20:32 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # telescope-fzy-native.nvim
+
 FZY style sorter that is compiled
+
+## Installation
+
+It is possible that you will already have a compiled binary matching your system. You can find out information
+about compiling the binary at the implementation repo: https://github.com/romgrk/fzy-lua-native
+
+You can install this with your favorite package manager. Make sure that it installs git submodules if stuff isn't working!
+
+## Usage
+
+You can override the file sorter by default simply by adding
+
+```lua
+require('telescope').load_extension('fzy_native')
+```
+
+somewhere after your `require('telescope').setup()` call.
---
+++ lua/telescope/_extensions/fzy_native.lua
@@ -0,0 +1,34 @@
+local if_nil = function(x, y)
+  if x == nil then return y end
+  return x
+end
+
+local native_lua = vim.api.nvim_get_runtime_file("deps/fzy-lua-native/lua/native.lua", false)[1]
+if not native_lua then
+  error("Unable to find native fzy native lua dep file. Probably need to update submodules!")
+end
+
+local native_lua_mod = loadfile(native_lua)()
+
+return require('telescope').register_extension {
+  setup = function(ext_config, config)
+    local override_file = if_nil(ext_config.override_file_sorter, true)
+
+    if override_file then
+      config.file_sorter = function()
+        return require('telescope.sorters').get_fzy_sorter({ fzy_mod = native_lua_mod })
+      end
+    end
+  end,
+
+  -- requires = ...
+  -- mappings = ...
+  -- actions = ...
+  -- commands = ...
+
+  exports = {
+    native_fzy_sorter = function()
+      return require('telescope.sorters').get_fzy_sorter({ fzy_mod = native_lua_mod })
+    end
+  }
+}
